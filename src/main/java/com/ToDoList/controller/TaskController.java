db69fc14288c7f54654f4af23b91cf79f28b66c9
Anastasiia Siutkina <sutckina.n@gmail.com>
2023-04-23 19:54:19 +0200

    some fixes

--- src/main/java/com/ToDoList/controller/TaskController.java
+++ src/main/java/com/ToDoList/controller/TaskController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 @AllArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 class TaskController {
     ToDoAPI toDoAPI;
     @GetMapping(value = "/tasks")
